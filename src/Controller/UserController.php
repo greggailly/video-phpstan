02381f9978248f9839b78cbdd0592c1747a89dcf
greggailly <greggailly@yahoo.fr>
2020-04-24 15:06:51 +0200

    Revert "basic project"
    This reverts commit 6e99e45356354fa1915eddefb4edad760ef544ae.

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,11 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\User;
-use App\Form\UserType;
 use App\Repository\UserRepository;
 use Doctrine\ORM\EntityRepository;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -17,7 +14,7 @@ class UserController extends AbstractController
 
     /**
      * Index
-     * @route("/", name="users")
+     *
      * @return Response
      */
     public function index()
@@ -25,83 +22,10 @@ class UserController extends AbstractController
         /** @var EntityRepository<User> */
         $repo = $this->getDoctrine()->getRepository(User::class);
 
-        $users = $repo->findAll();
+        $users = $repo->findByName('Greg');
 
         return $this->render('user/index.html.twig', [
-            'users' => $users
+            'controller_name' => 'UserController',
         ]);
-    }
-
-    /**
-     * Create a new user
-     * 
-     * @Route("/create", name="user_create")
-     *
-     * @param EntityManagerInterface $manager
-     * @param Request $request
-     * @return Response
-     */
-    public function create(EntityManagerInterface $manager, Request $request)
-    {
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager->persist($user);
-            $manager->flush();
-
-            return $this->redirectToRoute('users');
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * Edit existing user
-     *
-     * @Route("/edit/{id}", name="user_edit")
-     * 
-     * @param User $user
-     * @param EntityManagerInterface $manager
-     * @param Request $request
-     * @return Response
-     */
-    public function edit(User $user, EntityManagerInterface $manager, Request $request)
-    {
-        $form = $this->createForm(UserType::class, $user);
-
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $manager->persist($user);
-            $manager->flush();
-
-            return $this->redirectToRoute('users');
-        }
-
-        return $this->render('user/edit.html.twig', [
-            'form' => $form->createView()
-        ]);
-    }
-
-    /**
-     * Delete user
-     * 
-     * @Route("/delete/{id}", name="user_delete")
-     *
-     * @param User $user
-     * @param EntityManagerInterface $manager
-     * @return Response
-     */
-    public function delete(User $user, EntityManagerInterface $manager)
-    {
-        $manager->remove($user);
-        $manager->flush();
-
-        return $this->redirectToRoute('users');
     }
 }
